795d817fe4d550c44d13719ab7e57f8e55f70790
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-30 16:00:57 -0400

    Changed fields for cargo dto to be the same as the database's.

--- src/dtos/cargo.ts
+++ src/dtos/cargo.ts
@@ -1,16 +1,20 @@
 //Don't need to have Cargo user since that will be a state maintained with the user
 export class Cargo {
 
+    good_id: number;
     good_name: string;
     good_quantity: number;
     good_base_price: number;
 
     constructor(
+
+        good_id: number,
         good_name: string,
         good_quantity: number,
         base_price: number,
 
     ) {
+        this.good_id = good_id;
         this.good_name = good_name;
         this.good_quantity = good_quantity;
         this.good_base_price = base_price;
