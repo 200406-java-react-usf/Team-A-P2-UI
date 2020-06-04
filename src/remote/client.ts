398842e4b5f1ac891cae1c57a7b95f5cd8cb15ce
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-06-04 10:12:36 -0400

    Added /p2 to the axios url link for testing.

--- src/remote/client.ts
+++ src/remote/client.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const Client = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://localhost:8080/p2',
     headers: {
         'Content-Type': 'application/json'
     }
