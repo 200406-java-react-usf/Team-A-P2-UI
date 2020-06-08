9c95da8b8742a10cc1acf03e691c80cff2f6bc69
Scotty Thoms <spthoms19@gmail.com>
2020-06-08 01:00:31 -0500

    big dumb

--- src/remote/client.ts
+++ src/remote/client.ts
@@ -3,8 +3,8 @@
 import axios from 'axios';
 
 export const Client = axios.create({
-    baseURL: 'http://localhost:8080/p2',
-    // baseURL : 'http://team-a-p2.us-east-2.elasticbeanstalk.com/',
+    // baseURL: 'http://localhost:8080/p2',
+    baseURL : 'http://team-a-p2.us-east-2.elasticbeanstalk.com/',
     headers: {
         'Content-Type': 'application/json'
     }
